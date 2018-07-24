79a18e046b65fc08d8c1530d8068977b4993909e
Andrea Busi <andrea.busi@comelit.it>
2018-07-24 17:55:01 +0200

    improved logs

--- code/fakeRPi/GPIO.py
+++ code/fakeRPi/GPIO.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Fake class to simulate RPi.GPIO library outside a Raspberry PI
 """
@@ -10,26 +12,28 @@ HIGH = 1
 LOW = 0
 
 
-def setmode(a):
-    print('Set mode: ' + a)
+def setmode(mode):
+    print('[fakeRPi.GPIO] Set mode: ' + mode)
 
 
-def setup(a, b):
-    print('Set pin %s as %s' % (a, b))
+def setup(pin, state):
+    print('[fakeRPi.GPIO] Set pin %s as %s' % (pin, state))
 
 
-def output(a, b):
-    print('Output %s to pin %s' % (b, a))
+def output(pin, state):
+    print('[fakeRPi.GPIO] Output %s to pin %s' % (state, pin))
+
 
 def input(pin):
     state = random.choice([HIGH, LOW])
-    print('Input for pin %s is %s' % (pin, state))
+    print('[fakeRPi.GPIO] Input for pin %s is %s' % (pin, state))
     return state
 
 
 def cleanup():
-    print('a')
+    print('[fakeRPi.GPIO] Cleanup')
 
 
 def setwarnings(flag):
-    print('Set warnings as %s' % flag)
+    print('[fakeRPi.GPIO] Set warnings as %s' % flag)
+
