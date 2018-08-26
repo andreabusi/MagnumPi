5cd681da430bc4ea6892cb42a31a32d0a6e0873f
Andrea Busi <andrea.busi@comelit.it>
2018-08-26 12:20:35 +0200

    added ability to automatically reload templates when debugging app

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -4,6 +4,7 @@ from redis import Redis
 import rq
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config.from_object(Config)
 app.redis = Redis.from_url(app.config['REDIS_URL'])
 app.task_queue = rq.Queue(app.config['QUEUE_BACKGROUND_TASKS'], connection=app.redis)
