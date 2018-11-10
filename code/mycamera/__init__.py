ede766550513dcb20d2e9d69af6fd11ad9b4294e
Andrea Busi <busi.andrea@gmail.com>
2018-11-10 18:07:21 +0100

    added implementation to show live video from a PiCamera (or a simulated one)

---
+++ code/mycamera/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package allows to use a PiCamera or simulate it if it's not available.
+
+Source code is made by miguelgrinberg. Here is original links:
+
+* GitHub: https://github.com/miguelgrinberg/flask-video-streaming/tree/v1
+* Blog post: https://blog.miguelgrinberg.com/post/flask-video-streaming-revisited
+
+The code is distributed under MIT License.
+"""
