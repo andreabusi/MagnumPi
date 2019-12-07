ea965415677a6528ec72cd76a14a2e8688235dbe
Andrea Busi <busi.andrea@gmail.com>
2019-12-07 22:59:52 +0100

    Remove unused import and fix code style

--- code/app/forms.py
+++ code/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, BooleanField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 
